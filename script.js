25a665cd14b85cfea229cd06913405a4e92b7171
Herbert Guarezi <herbertguarezi@gmail.com>
2023-07-04 11:03:37 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll(".js-scroll");
+console.log(sections);
+const windowMetade = window.innerHeight * 0.6;
+function animaScroll() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const isSectionViseble = (sectionTop - windowMetade) < 0;
+    if (isSectionViseble) {
+      section.classList.add("ativo");
+    }
+  });
+}
+animaScroll();
+window.addEventListener('scroll', animaScroll)
