17a2e1d91a7433b2d263fe5411e379bba3c8df97
Herbert Guarezi <herbertguarezi@gmail.com>
2023-07-04 11:57:29 -0300

    Melhorado Scroll

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const sections = document.querySelectorAll(".js-scroll");
 console.log(sections);
-const windowMetade = window.innerHeight * 0.6;
+const windowMetade = window.innerHeight * 0.8;
 function animaScroll() {
   sections.forEach((section) => {
     const sectionTop = section.getBoundingClientRect().top;
